ffc3eebf69a3d537c6184d4505576b8de5365d75
Mahdi Sabahi <mrmahdisabahi1@gmail.com>
2025-07-24 02:20:43 +0330

    feat(watcher): use fs.open to read and log file content on changes

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-import { watch } from "node:fs/promises";
+import { watch, open } from "node:fs/promises";
+
+import { Buffer } from "node:buffer";
+
 const __filename = "command.txt";
 
 const ac = new AbortController();
 const { signal } = ac;
-setTimeout(() => ac.abort(), 10000);
+setTimeout(() => ac.abort(), 100000);
 
 (async () => {
+  const commandFileHandler = await open("./command.txt", "r");
+
   try {
     const watcher = watch(__filename, { signal });
     for await (const event of watcher) {
-      console.log(`[${new Date().toLocaleTimeString()}]`, event);
+      const size = (await commandFileHandler.stat()).size;
+      const buff = Buffer.alloc(size);
+      const offset = 0;
+      const length = buff.byteLength;
+      const position = 0;
+      const contentFile = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      if (event.eventType === "change") {
+        console.log(contentFile.buffer.toString("utf-8"));
+      }
     }
   } catch (err) {
     console.log("i am logging");
